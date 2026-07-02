56ac68e8f155b8e0355111e3b1c49e22e2502e56
Creator addons dannys <abidayman195@gmail.com>
2026-07-02 21:24:34 +0000

    Fix Camera setPos access

--- src/client/java/com/dannysaot/colossalmod/client/mixin/TitanCameraMixin.java
+++ src/client/java/com/dannysaot/colossalmod/client/mixin/TitanCameraMixin.java
@@ -6,17 +6,17 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Camera.class)
-public class TitanCameraMixin {
+public abstract class TitanCameraMixin {
 
-    /**
-     * When the local player is riding Armin's Colossal, anchor the camera
-     * to follow the titan's head bone position instead of the player's body.
-     */
+    @Shadow
+    protected abstract void setPos(double x, double y, double z);
+
     @Inject(method = "update", at = @At("TAIL"))
     private void onCameraUpdate(BlockView area, Entity focusedEntity,
                                 boolean thirdPerson, boolean inverseView,
@@ -27,9 +27,7 @@ public class TitanCameraMixin {
         Entity vehicle = client.player.getVehicle();
         if (!(vehicle instanceof ArminsColossalEntity titan)) return;
 
-        // Position camera at titan head level (roughly 85% up the titan's height)
-        Camera camera = (Camera)(Object)this;
         double headY = titan.getY() + titan.getHeight() * 0.85;
-        camera.setPos(titan.getX(), headY, titan.getZ());
+        setPos(titan.getX(), headY, titan.getZ());
     }
 }
